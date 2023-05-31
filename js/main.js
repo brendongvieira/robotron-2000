ace81f1e2dd68512e315b0e72b7aa8e43b5b69d6
Brendon <brendon-vieira@live.com>
2023-05-30 22:16:23 -0300

    Adicionando manipulação das peças

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 const subtrair = document.querySelector("#subtrair");
 const somar = document.querySelector("#somar");
-const braco = document.querySelector("#braco");
 
-somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) + 1;
-});
+const controle = document.querySelectorAll(".controle-ajuste");
 
-subtrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) - 1;
-});
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode);
+     });
+    });
+
+function manipulaDados(operacao, controle) {
+
+    const peca = controle.querySelector(".controle-contador");
+
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
+    } else {
+        peca.value = parseInt(peca.value) + 1;
+    }
+}
